63f4b40178e087620609e93655303a3cb6aa7087
Radek <r.skupnik@gmail.com>
2024-09-25 13:11:14 +0200

    Session handling

---
+++ src/main/java/com/github/rskupnik/tpl/SessionChangeDTO.java
@@ -0,0 +1,14 @@
+package com.github.rskupnik.tpl;
+
+public class SessionChangeDTO {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
--- src/main/java/com/github/rskupnik/tpl/WebController.java
+++ src/main/java/com/github/rskupnik/tpl/WebController.java
@@ -8,7 +8,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class WebController {
@@ -24,40 +29,64 @@ public class WebController {
 
     @RequestMapping(path = {"/", "/mapa"})
     public String map(HttpServletRequest request, Model model, HttpServletResponse response) {
-        var cookies = request.getCookies();
-        if (cookies == null || cookies.length == 0 || !sessionIdCookieExists(cookies)) {
-            setNewSessionCookie(response);
+        return handleSession(request, response, model).orElse("pages/map");
+    }
+
+    @RequestMapping("/evidence")
+    public String evidence(HttpServletRequest request, Model model, HttpServletResponse response) {
+        return handleSession(request, response, model).orElse("pages/evidence");
+    }
+
+    @RequestMapping("/session-change")
+    public String sessionChange(HttpServletRequest request, Model model, HttpServletResponse response) {
+        var errorRouting = handleSession(request, response, model);
+        if (errorRouting.isPresent()) {
+            return errorRouting.get();
+        }
+
+        model.addAttribute("sessionChange", new SessionChangeDTO());
+        return "pages/sessionChange";
+    }
+
+    @PostMapping("/session-change/exec")
+    public String executeSessionChange(@ModelAttribute SessionChangeDTO sessionChange, Model model, HttpServletResponse response) {
+        var session = sessionService.getSession(sessionChange.getId().trim().toUpperCase());
+        if (session.isEmpty()) {
+            logger.warn("Session not found when trying to change session: {}", sessionChange.getId());
+            // TODO: Feedback to user?
             return "pages/begin";
         }
 
+        Cookie sessionIdCookie = new Cookie("sessionId", sessionChange.getId());
+        sessionIdCookie.setPath("/");
+        response.addCookie(sessionIdCookie);
+
+        model.addAttribute("sessionChange", sessionChange);
+        model.addAttribute("userSession", SessionDto.fromEntity(session.get()));
+
+        // TODO: Feedback to user
+        return "pages/sessionChange";
+    }
+
+    private Optional<String> handleSession(HttpServletRequest request, HttpServletResponse response, Model model) {
+        var cookies = request.getCookies();
+        if (cookies == null || cookies.length == 0 || !sessionIdCookieExists(cookies)) {
+            setNewSessionCookie(response);
+            return Optional.of("pages/begin");
+        }
+
         var sessionIdCookie = retrieveCookie("sessionId", cookies);
-        var session = sessionService.getSession(sessionIdCookie.getValue());
+        var session = sessionService.getSession(sessionIdCookie.getValue().trim().toUpperCase());
         if (session.isEmpty()) {
             // Invalid session, generate a new one?
             logger.warn("Session not found: {}", sessionIdCookie.getValue());
             setNewSessionCookie(response);
-            return "pages/begin";
+            return Optional.of("pages/begin");
         }
 
-        model.addAttribute("session", session);
+        model.addAttribute("userSession", SessionDto.fromEntity(session.get()));
 
-        return "pages/map";
-    }
-
-    @RequestMapping("/begin")
-    public String begin(HttpServletResponse response, Model model) {
-        var session = sessionService.generateSession();
-        System.out.println("Session generated");
-        Cookie sessionIdCookie = new Cookie("sessionId", session.getId());
-        sessionIdCookie.setPath("/");
-        response.addCookie(sessionIdCookie);
-        return "/";
-    }
-
-    @RequestMapping("/evidence")
-    public String evidence(Model model) {
-        //model.addAttribute("hello", "world");
-        return "pages/evidence";
+        return Optional.empty();
     }
 
     private void setNewSessionCookie(HttpServletResponse response) {
